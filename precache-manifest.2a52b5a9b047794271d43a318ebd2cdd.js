52c363a320bf76e8946b7d05c380e1512e3f54cb
EduPassero <edupassero47@gmail.com>
2023-02-04 00:30:11 -0300

    Updates

---
+++ precache-manifest.2a52b5a9b047794271d43a318ebd2cdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f96f14f6c5d91646811",
+    "url": "/ep_calculator/static/css/main.770fab3e.chunk.css"
+  },
+  {
+    "revision": "7f96f14f6c5d91646811",
+    "url": "/ep_calculator/static/js/main.7f96f14f.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/ep_calculator/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/ep_calculator/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "9bccca3a3691d0b3a588",
+    "url": "/ep_calculator/static/js/runtime~main.9bccca3a.js"
+  },
+  {
+    "revision": "b29b2254c7842558a9b2f9ddc5d6d2fa",
+    "url": "/ep_calculator/index.html"
+  }
+];
